b22d1a205bbd5cae64c586df998d79d1066737de
primez-x <primez-x@users.noreply.github.com>
2026-07-24 19:00:32 -0600

    Fix watchlist discover API headers + add watchlist_add_key mode for non-library items (v3.12.30)

---
+++ context_watchlist_remove_key.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Grabs the Plex ratingKey for a non-library (discover) item the context menu was
+called for and sends a request to our main Python instance to remove it from the
+Plex Watchlist.
+"""
+
+from urllib.parse import urlencode
+
+from xbmc import sleep
+from xbmcgui import Window
+
+
+def main():
+    import xbmc
+
+    rating_key = xbmc.getInfoLabel("ListItem.Property(ratingKey)")
+    plex_type = xbmc.getInfoLabel("ListItem.DBTYPE")
+    if not rating_key:
+        return
+    args = {"rating_key": rating_key, "plex_type": plex_type}
+    window = Window(10000)
+    while window.getProperty("plexkodiconnect.command"):
+        sleep(20)
+    window.setProperty(
+        "plexkodiconnect.command", "WATCHLIST_REMOVE_KEY?%s" % urlencode(args)
+    )
+
+
+if __name__ == "__main__":
+    main()
--- default.py
+++ default.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 from sys import argv
-from urllib.parse import parse_qsl
+from urllib.parse import parse_qsl, urlencode
 
 import xbmc
 import xbmcgui
@@ -9,127 +9,139 @@ import xbmcplugin
 
 from resources.lib import entrypoint, utils, transfer, variables as v, loghandler
 
-
 loghandler.config()
-LOG = logging.getLogger('PLEX.default')
+LOG = logging.getLogger("PLEX.default")
 
 
 def triage(mode, params, path, arguments, itemid):
-    if mode == 'play':
+    if mode == "play":
         play()
         return
-    elif mode == 'plex_node':
+    elif mode == "plex_node":
         play()
         return
-    elif mode == 'route_to_extras':
+    elif mode == "route_to_extras":
         # Hack so we can store this path in the Kodi DB
-        handle = ('plugin://%s?mode=extras&plex_id=%s'
-                  % (v.ADDON_ID, params.get('plex_id')))
-        xbmc.executebuiltin('ActivateWindow(videos,\"%s\",return)' % handle)
+        handle = "plugin://%s?mode=extras&plex_id=%s" % (
+            v.ADDON_ID,
+            params.get("plex_id"),
+        )
+        xbmc.executebuiltin('ActivateWindow(videos,"%s",return)' % handle)
         return
-    elif mode == 'settings':
-        xbmc.executebuiltin('Addon.OpenSettings(%s)' % v.ADDON_ID)
+    elif mode == "settings":
+        xbmc.executebuiltin("Addon.OpenSettings(%s)" % v.ADDON_ID)
         return
-    elif mode == 'skip_marker':
-        transfer.plex_command('skip-marker')
+    elif mode == "skip_marker":
+        transfer.plex_command("skip-marker")
         return
-    elif mode == 'enterPMS':
-        LOG.info('Request to manually enter new PMS address')
-        transfer.plex_command('enter_new_pms_address')
+    elif mode == "enterPMS":
+        LOG.info("Request to manually enter new PMS address")
+        transfer.plex_command("enter_new_pms_address")
         return
-    elif mode == 'reset':
-        transfer.plex_command('RESET-PKC')
+    elif mode == "reset":
+        transfer.plex_command("RESET-PKC")
         return
-    elif mode == 'togglePlexTV':
-        LOG.info('Toggle of Plex.tv sign-in requested')
-        transfer.plex_command('toggle_plex_tv_sign_in')
+    elif mode == "togglePlexTV":
+        LOG.info("Toggle of Plex.tv sign-in requested")
+        transfer.plex_command("toggle_plex_tv_sign_in")
         return
-    elif mode == 'passwords':
+    elif mode == "passwords":
         from resources.lib.windows import direct_path_sources
+
         direct_path_sources.start()
         return
-    elif mode == 'switchuser':
-        LOG.info('Plex home user switch requested')
-        transfer.plex_command('switch_plex_user')
+    elif mode == "switchuser":
+        LOG.info("Plex home user switch requested")
+        transfer.plex_command("switch_plex_user")
         return
-    elif mode in ('manualsync', 'repair'):
-        if mode == 'repair':
-            LOG.info('Requesting repair lib sync')
-            transfer.plex_command('repair-scan')
-        elif mode == 'manualsync':
-            LOG.info('Requesting full library scan')
-            transfer.plex_command('full-scan')
+    elif mode in ("manualsync", "repair"):
+        if mode == "repair":
+            LOG.info("Requesting repair lib sync")
+            transfer.plex_command("repair-scan")
+        elif mode == "manualsync":
+            LOG.info("Requesting full library scan")
+            transfer.plex_command("full-scan")
         return
-    elif mode == 'texturecache':
-        LOG.info('Requesting texture caching of all textures')
-        transfer.plex_command('textures-scan')
+    elif mode == "texturecache":
+        LOG.info("Requesting texture caching of all textures")
+        transfer.plex_command("textures-scan")
         return
-    elif mode == 'chooseServer':
+    elif mode == "chooseServer":
         LOG.info("Choosing PMS server requested, starting")
-        transfer.plex_command('choose_pms_server')
+        transfer.plex_command("choose_pms_server")
         return
-    elif mode == 'deviceid':
-        LOG.info('New PKC UUID / unique device id requested')
-        transfer.plex_command('generate_new_uuid')
+    elif mode == "deviceid":
+        LOG.info("New PKC UUID / unique device id requested")
+        transfer.plex_command("generate_new_uuid")
         return
-    elif mode == 'fanart':
-        LOG.info('User requested fanarttv refresh')
-        transfer.plex_command('fanart-scan')
+    elif mode == "fanart":
+        LOG.info("User requested fanarttv refresh")
+        transfer.plex_command("fanart-scan")
+        return
+    elif mode == "watchlist_add_key":
+        transfer.plex_command("WATCHLIST_ADD_KEY?%s" % urlencode(params))
+        return
+    elif mode == "watchlist_remove_key":
+        transfer.plex_command("WATCHLIST_REMOVE_KEY?%s" % urlencode(params))
         return
     # Listings: we list ListItems and need to tell Kodi when we're done
     try:
-        if mode == 'browseplex':
-            entrypoint.browse_plex(key=params.get('key'),
-                                   plex_type=params.get('plex_type'),
-                                   section_id=params.get('section_id'),
-                                   synched=params.get('synched') != 'false',
-                                   prompt=params.get('prompt'),
-                                   query=params.get('query'))
-        elif mode == 'show_section':
-            entrypoint.show_section(params.get('section_index'))
-        elif mode == 'watchlater':
+        if mode == "browseplex":
+            entrypoint.browse_plex(
+                key=params.get("key"),
+                plex_type=params.get("plex_type"),
+                section_id=params.get("section_id"),
+                synched=params.get("synched") != "false",
+                prompt=params.get("prompt"),
+                query=params.get("query"),
+            )
+        elif mode == "show_section":
+            entrypoint.show_section(params.get("section_index"))
+        elif mode == "watchlater":
             entrypoint.watchlater()
-        elif mode == 'watchlist':
-            entrypoint.watchlist(section_id=params.get('section_id'))
-        elif mode == 'channels':
-            entrypoint.browse_plex(key='/channels/all')
-        elif mode == 'search':
+        elif mode == "watchlist":
+            entrypoint.watchlist(section_id=params.get("section_id"))
+        elif mode == "channels":
+            entrypoint.browse_plex(key="/channels/all")
+        elif mode == "search":
             # "Search"
-            entrypoint.browse_plex(key='/hubs/search',
-                                   args={'includeCollections': 1,
-                                         'includeExternalMedia': 1},
-                                   prompt=utils.lang(137),
-                                   query=params.get('query'))
-        elif mode == 'route_to_extras':
+            entrypoint.browse_plex(
+                key="/hubs/search",
+                args={"includeCollections": 1, "includeExternalMedia": 1},
+                prompt=utils.lang(137),
+                query=params.get("query"),
+            )
+        elif mode == "route_to_extras":
             # Hack so we can store this path in the Kodi DB
-            handle = ('plugin://%s?mode=extras&plex_id=%s'
-                      % (v.ADDON_ID, params.get('plex_id')))
-            xbmc.executebuiltin('ActivateWindow(videos,\"%s\",return)' % handle)
+            handle = "plugin://%s?mode=extras&plex_id=%s" % (
+                v.ADDON_ID,
+                params.get("plex_id"),
+            )
+            xbmc.executebuiltin('ActivateWindow(videos,"%s",return)' % handle)
 
-        elif mode == 'extras':
-            entrypoint.extras(plex_id=params.get('plex_id'))
+        elif mode == "extras":
+            entrypoint.extras(plex_id=params.get("plex_id"))
         # Called by e.g. 3rd party plugin video extras
-        elif ('/Extras' in path or '/VideoFiles' in path or
-                '/Extras' in arguments):
+        elif "/Extras" in path or "/VideoFiles" in path or "/Extras" in arguments:
             plexId = itemid or None
             entrypoint.get_video_files(plexId, params)
-        elif mode == 'playlists':
-            entrypoint.playlists(params.get('content_type'))
-        elif mode == 'hub':
-            entrypoint.hub(params.get('content_type'))
-        elif mode == 'select-libraries':
-            LOG.info('User requested to select Plex libraries')
-            transfer.plex_command('select-libraries')
-        elif mode == 'refreshplaylist':
-            LOG.info('User requested to refresh Kodi playlists and nodes')
-            transfer.plex_command('refreshplaylist')
-        elif '/extrafanart' in path:
+        elif mode == "playlists":
+            entrypoint.playlists(params.get("content_type"))
+        elif mode == "hub":
+            entrypoint.hub(params.get("content_type"))
+        elif mode == "select-libraries":
+            LOG.info("User requested to select Plex libraries")
+            transfer.plex_command("select-libraries")
+        elif mode == "refreshplaylist":
+            LOG.info("User requested to refresh Kodi playlists and nodes")
+            transfer.plex_command("refreshplaylist")
+        elif "/extrafanart" in path:
             plexpath = arguments[1:]
             plexid = itemid
             entrypoint.extra_fanart(plexid, plexpath)
             entrypoint.get_video_files(plexid, plexpath)
         else:
-            entrypoint.show_main_menu(content_type=params.get('content_type'))
+            entrypoint.show_main_menu(content_type=params.get("content_type"))
     except entrypoint.ListingException:
         # Tell Kodi listing is done and we failed
         xbmcplugin.endOfDirectory(int(argv[1]), False)
@@ -142,42 +154,42 @@ def play():
     """
     Start up playback_starter in main Python thread
     """
-    request = '%s&handle=%s' % (argv[2], int(argv[1]))
+    request = "%s&handle=%s" % (argv[2], int(argv[1]))
     # Put the request into the 'queue'
-    transfer.plex_command('PLAY-%s' % request)
+    transfer.plex_command("PLAY-%s" % request)
     if int(argv[1]) == -1:
         # Handle -1 received, not waiting for main thread
         return
     # Wait for the result from the main PKC thread
-    result = transfer.wait_for_transfer(source='main')
+    result = transfer.wait_for_transfer(source="main")
     if result is True:
         xbmcplugin.setResolvedUrl(int(argv[1]), False, xbmcgui.ListItem())
         # Tell main thread that we're done
-        transfer.send(True, target='main')
+        transfer.send(True, target="main")
     else:
         # Received a xbmcgui.ListItem()
         xbmcplugin.setResolvedUrl(int(argv[1]), True, result)
 
 
 def main():
-    LOG.debug('Full sys.argv received: %s', argv)
+    LOG.debug("Full sys.argv received: %s", argv)
     # Parse parameters that were passed
     params = dict(parse_qsl(argv[2][1:]))
-    mode = params.get('mode', '')
+    mode = params.get("mode", "")
     path = argv[0]
     arguments = argv[2]
-    itemid = params.get('id', '')
+    itemid = params.get("id", "")
     triage(mode, params, path, arguments, itemid)
 
 
-if __name__ == '__main__':
-    LOG.info('%s started' % v.ADDON_ID)
+if __name__ == "__main__":
+    LOG.info("%s started" % v.ADDON_ID)
     try:
         v.database_paths()
     except RuntimeError as err:
         # Database does not exists
-        LOG.error('The current Kodi version is incompatible')
-        LOG.error('Error: %s', err)
+        LOG.error("The current Kodi version is incompatible")
+        LOG.error("Error: %s", err)
     else:
         main()
-    LOG.info('%s stopped' % v.ADDON_ID)
+    LOG.info("%s stopped" % v.ADDON_ID)
